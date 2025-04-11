6af3ebbf154bf8d9fcb6339aedf750ea023854f3
anik-das <anikdas1085@gmail.com>
2025-04-11 23:40:50 +0600

    shopping cart

---
+++ collection.py
@@ -0,0 +1,24 @@
+
+fruits = ("apple","orange","banana","coconut")
+
+# print(fruits[::-1])
+# fruits[0] = "pineapple"
+# fruits.append("pineapple")
+# fruits.remove("coconut")
+# fruits.insert(0,"pineapple")
+# fruits.sort()
+
+# for fruit in fruits:
+#     print(fruit)
+
+# print(dir(fruits))
+# print(help(fruits))
+# print("apple" in fruits)
+
+
+# print(len(fruits))
+
+# print(fruits.count("apple"))
+
+for fruit in fruits:
+    print(fruit)
